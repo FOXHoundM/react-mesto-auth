09f4bb3a90fa5e4bc0b001aaf16af172ac2522ec
Mikael Gulmamedov <4216623@gmail.com>
2022-11-18 15:04:26 +0300

    project 12

---
+++ src/components/Login.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import Header from "./Header";
+
+const Login = ({handleLogin}) => {
+
+	const [authData, setAuthData] = useState({email: '', password: ''})
+
+	const handleChange = (evt) => {
+		const {name, value} = evt.target
+		setAuthData({...authData, [name]: value})
+	}
+
+	const handleLoginSubmit = (evt) => {
+		evt.preventDefault()
+		handleLogin(authData)
+	}
+
+	return (
+		<>
+			<Header link="/sign-up" headerText="Регистрация" />
+		<div className="authorization">
+			<h2 className="authorization__title">Вход</h2>
+
+			<form className="authorization__form" onSubmit={handleLoginSubmit}>
+				<input
+					autoComplete="off"
+					value={authData.email}
+					type="email"
+					className="authorization__input"
+					required
+					name="email"
+					id="email"
+					placeholder="E-mail"
+					onChange={handleChange}
+				/>
+				<input
+					autoComplete="off"
+					value={authData.password}
+					type="password"
+					className="authorization__input"
+					required
+					name="password"
+					id="password"
+					placeholder="Пароль"
+					onChange={handleChange}
+				/>
+
+				<button
+					type="submit"
+					className="authorization__submit"
+				>
+					Войти
+				</button>
+			</form>
+
+		</div>
+		</>
+	)
+}
+
+export default Login
